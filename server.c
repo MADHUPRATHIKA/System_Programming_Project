53516baadd3b0fc6a69588c8165b8f57148ac5ac
MADHUPRATHIKA <madhuprathika24@gmail.com>
2026-05-25 23:40:46 +0530

    Socket Assignment

---
+++ server.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    char buffer[1024] = {0};
+    char *message = "Hello Client";
+
+    /* Create socket */
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd == 0)
+    {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Address configuration */
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    /* Bind socket */
+    if (bind(server_fd,
+             (struct sockaddr *)&address,
+             sizeof(address)) < 0)
+    {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Listen for connections */
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("Listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server waiting for connection...\n");
+
+    /* Accept client */
+    new_socket = accept(server_fd,
+                        (struct sockaddr *)&address,
+                        (socklen_t *)&addrlen);
+
+    if (new_socket < 0)
+    {
+        perror("Accept");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Print client IP */
+    printf("Client Connected: %s\n",
+           inet_ntoa(address.sin_addr));
+
+    /* Receive message */
+    recv(new_socket, buffer, sizeof(buffer), 0);
+
+    printf("Received: %s\n", buffer);
+
+    /* Send reply */
+    send(new_socket, message, strlen(message), 0);
+
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
